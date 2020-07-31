1ace6aee9a537b21e9b3da58cea8086921939e23
Konstantin <example@gmail.com>
2020-07-31 15:55:58 +0300

    first commit

---
+++ jssctript/script.js
@@ -0,0 +1,50 @@
+let number = 2.2;
+console.log(-4/0);
+console.log('string' * 9)
+
+const persone = "Alex";
+
+const bool = false;
+
+const obj = {
+    name : "Alex",
+    age : "25",
+    isMarried: false
+};
+// console.log(obj.name);
+console.log(obj["name"]);
+
+let arr = ['plum.png', 'orange.jpeg', 6 , 'apple.bmp'];
+console.log(arr[1]);
+
+// alert ('Hello');
+ //const result = confirm("Are you here?");
+ //console.log(result);
+ // const answer = +prompt("Вам есть 18?", "18")
+ // console.log(answer+5);
+ const answers = [];
+
+//  answers [0] = prompt ('Как вас зовут?', '');
+//  answers [1] = prompt ('Как ваше фамилия?', '');
+//  answers [2] = prompt ('Сколько вам лет?', '');
+//  document.write(typeof(answers));
+const category = 'toys';
+console.log(`https://someurl.com/${category}/5`);
+const user = "Misha";
+alert (`Привет,${user}`);
+
+console.log(4 + "5");
+let incr = 10,
+    decr = 10;
+
+// ++incr;
+// --decr;
+console.log(++incr);
+console.log(--decr);
+console.log(5%2);
+console.log(2*4 === 8);
+
+const isChecked = true,
+      isClose = false;
+     
+ console.log(isChecked || isClose);
